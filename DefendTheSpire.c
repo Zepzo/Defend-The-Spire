9085af8e1b3af0529c9bfdee33405001801088d8
Zätha Malmberg <zathamalmberg@gmail.com>
2025-03-11 19:53:50 +0100

    added a creep that i can move around

--- DefendTheSpire.c
+++ DefendTheSpire.c
@@ -1,20 +1,20 @@
 /* Todo list
 
 * set up
-- read the red blob games articels
-- make a grid/ tile map
+- read the red blob games articels. done
+- make a grid/ tile map. done
 
 * pathfinding
-- chouse algorithm(Breadth-first search)
-- implement it
+- chouse algorithm(Breadth-first search). done
+- implement it.
 
 * creeps
-- creeps can spawn and move in the grid
-- creeps follow the pathfinding
-- more than one kind?
+- creeps can spawn and move in the grid.
+- creeps follow the pathfinding.
+- more than one kind?.
 
 * tiles
-- difrent tiels?
+- difrent tiels?.
 
 * towers? (idk if i will get to this)
 -fill out later
@@ -41,12 +41,33 @@ int main(void)
         }
     }
     
+    int circel_X = 1;
+    int circel_Y = 1;
+    
+    Vector2 circel = { grid[circel_Y][circel_X].x, grid[circel_Y][circel_X].y};
+    
     InitWindow(screenWidth, screenHeight, "Defend the spire");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        if (IsKeyPressed(KEY_D) && circel_X != GirdSize - 1){
+            circel_X++;
+            circel.x = grid[circel_Y][circel_X].x;
+        }
+        else if (IsKeyPressed(KEY_A) && circel_X != 0){
+            circel_X--;
+            circel.x = grid[circel_Y][circel_X].x;
+        }
+        else if (IsKeyPressed(KEY_W) && circel_Y != 0){
+            circel_Y--;
+            circel.y = grid[circel_Y][circel_X].y;
+        }
+        else if (IsKeyPressed(KEY_S) && circel_Y != GirdSize - 1){
+            circel_Y++;
+            circel.y = grid[circel_Y][circel_X].y;
+        }
         
         BeginDrawing();
 
@@ -58,6 +79,8 @@ int main(void)
                     DrawRectangleLines(grid[i][j].x, grid[i][j].y, grid[i][j].width, grid[i][j].height, WHITE);
                 }
             }
+            
+            DrawCircle(circel.x + 15, circel.y + 15, 10, RED);
 
         EndDrawing();
     }
