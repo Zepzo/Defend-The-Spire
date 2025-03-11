f36455322b6892f503c2b1720186ec9a7c92feb8
Zätha <zathamalmberg@gmail.com>
2025-03-11 13:27:25 +0100

    started on tilemap

--- DefendTheSpire.c
+++ DefendTheSpire.c
@@ -5,13 +5,16 @@
 - make a grid/ tile map
 
 * pathfinding
-- chouse algorithm
+- chouse algorithm(Breadth-first search)
 - implement it
 
 * creeps
 - creeps can spawn and move in the grid
 - creeps follow the pathfinding
 - more than one kind?
+
+* tiles
+- difrent tiels?
 
 * towers? (idk if i will get to this)
 -fill out later
@@ -25,6 +28,19 @@ int main(void)
     const int screenWidth = 900;
     const int screenHeight = 900;
     
+    Rectangle grid[50][50];
+    
+    for(int i = 0; i < 50; i++){
+        for(int j = 0; j < 50; j++){
+            grid[i][j].x = j * 10;
+            grid[i][j].y = i * 10;
+            grid[i][j].width = 20;
+            grid[i][j].height = 20;
+        }
+    }
+    
+    
+    
     InitWindow(screenWidth, screenHeight, "Defend the spire");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -35,6 +51,12 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
+            
+            for(int i = 0; i < 50; i++){
+                for(int j = 0; j < 50; j++){
+                    DrawRectangle(grid[i][j].x, grid[i][j].y, grid[i][j].width, grid[i][j].height, BLACK);
+                }
+            }
 
         EndDrawing();
     }
